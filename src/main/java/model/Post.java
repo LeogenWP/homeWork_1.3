f5669016029b449958ac6acf63bc0fe0aaacdaef
Gena Pankratov <gpankratov95@mail.ru>
2021-02-16 19:59:33 +0300

    add static int

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -3,6 +3,7 @@ package model;
 import java.util.List;
 
 public class Post {
+    private static int postId = 1;
     private int id;
     private String content;
     private String created;
--- src/main/java/model/Writer.java
+++ src/main/java/model/Writer.java
@@ -3,6 +3,7 @@ package model;
 import java.util.List;
 
 public class Writer {
+    private static int writerId = 1;
     private int id;
     private String firstName;
     private String lastName;
